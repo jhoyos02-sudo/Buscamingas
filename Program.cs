7a32864edb63a6302fd144488fc368613976a3d5
jhoyos02 <jhoyos02@ucm.es>
2026-03-26 09:58:35 +0100

    Codigo con metodos vacios

--- Program.cs
+++ Program.cs
@@ -1,7 +1,119 @@
-﻿namespace Buscamingas
+﻿using System.Drawing;
+using Coordinates;
+
+namespace Buscamingas
 {
     internal class Program
     {
+        class Tablero
+        {
+            // Casillas de juego
+            private struct Casilla
+            {
+                public char estado; // ’o’ sin descubrir, ’1’-’8’ número de minas adyacentes, ’*’ marcado como mina
+                                    // ’x’ mina descubierta/explotada, ’·’ descubierto sin minas adyacentes
+                public bool mina; // true: hay mina; false eoc
+            }
+            private int fils, cols; // numero de filas y columnas del tablero
+            private Casilla[,] casilla; // matriz de casillas del tablero
+            private Coor cursor; // posición del cursor
+            private int nMinas, nMarcadas; // número de minas y número de casillas marcadas
+            private bool primerClick; // para garantizar que el primer click no sea una mina
+            private bool debug; // para depuración, el Renderizado muestra las minas en el tablero
+            static Random rnd = new Random(); // generador de aleatorios para colocar las minas
+
+            public int Fils()
+            {
+                return fils;
+            }
+
+            public int Cols()
+            {
+                return cols;
+            }
+
+            public Tablero(int fils, int cols, int numMinas)
+            {
+
+            }
+
+            private void ponMinas1(int nMinas)
+            {
+
+            }
+
+            public Tablero(int fils, int cols, (int, int)[] posMinas)
+            {
+
+            }
+
+            public void activateDEBUG()
+            {
+
+            }
+
+            public void Render(bool bomba)
+            {
+
+            }
+
+            public void MueveCursor(Coor dir)
+            {
+
+            }
+
+            public void MarcaMina()
+            {
+
+            }
+
+            public bool ClickCasilla()
+            {
+
+                return true;
+            }
+
+            private int MinasAlrededor(int x, int y)
+            {
+
+            }
+
+            private void DescubreAdyacentes()
+            {
+
+            }
+
+            public bool Terminado()
+            {
+
+                return true;
+            }
+        }
+
+        bool ProcesaInput(Tablero t, char c) 
+        {
+
+            return true;
+        }
+
+        public static char LeeInput()
+        {
+            char d = ' ';
+            string tecla = Console.ReadKey(true).Key.ToString();
+            switch (tecla)
+            {
+                case "LeftArrow": d = 'l'; break; // izda
+                case "UpArrow": d = 'u'; break; // arriba
+                case "RightArrow": d = 'r'; break; // dcha
+                case "DownArrow": d = 'd'; break; // abajo
+                case "Spacebar": d = 'c'; break; // click para destapar
+                case "Enter": d = 'x'; break; // marca/desmarca mina
+                case "Escape": d = 'q'; break; // abandonar partida
+            }
+            while (Console.KeyAvailable) Console.ReadKey().Key.ToString();
+            return d;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
